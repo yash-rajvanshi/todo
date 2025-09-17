da12f8691f6bc51b83856ba079d0012be65e7749
Yash Rajvanshi <yash526452@gmail.com>
2025-09-17 13:33:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { Fab, TextField, Typography } from '@mui/material';
+
+const App = () => {
+  return (
+    <>b</>
+
+  )
+}
+
+export default App
---
+++ src/context/useAuth.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createContext } from "react";
+
+const AuthContext = createContext();
+
+
+export const AuthProvider = ({children}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useAuth
